9292dbd371d506b1be5d32561fb7860f59391592
Agit Firat Morcicek <agitfiratmorcicek@macbook-air-de-agit.home>
2024-04-30 00:29:14 +0200

    first commit

---
+++ pages/Expérience.py
@@ -0,0 +1,16 @@
+import streamlit as st
+ 
+
+st.header("Expérience")
+
+# Formation 1
+st.subheader("Apprenti Data Analyst (juil 2023 - sept 2024)")
+
+st.write("### Assurances du Crédit Mutuel")
+st.write("#### Strasbourg")
+st.write("""
+        - Traiter des données à des fins décisionnelles (SAS / Python)
+        - Analyser statistiquement des données (SAS / Python)
+        - Valoriser une production dans un contexte professionnel 
+        - Développer un outil décisionnel (Excel / Power Bi / Business Object)
+    """)
