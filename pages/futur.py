da12a5e7a9fbc997c958d2d7f6cdf9f48a138956
agitfirat <157006017+agitfirat@users.noreply.github.com>
2024-04-30 13:10:24 +0200

    Update futur.py

--- pages/futur.py
+++ pages/futur.py
@@ -1,6 +1,7 @@
 import streamlit as st 
 
-st.header("""'Personne ne peut prédire l'avenir, il est rempli d'incertitudes et de mystères. Ce qui rend la vie excitante, c'est l'inconnu qui nous attend demain.' chatGPT""")
+st.header("""FUTUR""")
+st.subheader("""'Personne ne peut prédire l'avenir, il est rempli d'incertitudes et de mystères. Ce qui rend la vie excitante, c'est l'inconnu qui nous attend demain.' chatGPT""")
 
 st.write("""Aujourd'hui l'homme est arrivé au point ou nous arrivons à faire dire ce genre de chose à des ia. 
          En choisisant cette formation j'avais un but en tête m'orienté ensuite vers le monde de l'ia, je voulais absolument
