ef87795a1c71ae2b7c5e1193dcd366df0fde3251
agitfirat <157006017+agitfirat@users.noreply.github.com>
2024-04-30 13:11:04 +0200

    Update futur.py

--- pages/futur.py
+++ pages/futur.py
@@ -1,6 +1,6 @@
 import streamlit as st 
 
-st.header("""FUTUR""")
+st.header("FUTUR")
 st.subheader("""'Personne ne peut prédire l'avenir, il est rempli d'incertitudes et de mystères. Ce qui rend la vie excitante, c'est l'inconnu qui nous attend demain.' chatGPT""")
 
 st.write("""Aujourd'hui l'homme est arrivé au point ou nous arrivons à faire dire ce genre de chose à des ia. 
