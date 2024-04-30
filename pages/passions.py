b11341d51e4d830cbc69764b8ea8326a9f040dfc
agitfirat <157006017+agitfirat@users.noreply.github.com>
2024-04-30 15:54:55 +0200

    Update passions.py

--- pages/passions.py
+++ pages/passions.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
+tab1, tab2 = st.tabs(["Automobile", "Football"])
 
+with tab1 :
+  st.header("Automobile")
+  st.write("""Depuis le premier jour où je suis montée dans une voiture sportive (Audi RS3), je suis passionnée par le monde automobile.
+  
+  """)
