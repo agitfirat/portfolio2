2f683d9745c0ee12ff88f3ebab9ed831e3ddb5af
agitfirat <157006017+agitfirat@users.noreply.github.com>
2024-04-30 16:26:08 +0200

    Update passions.py

--- pages/passions.py
+++ pages/passions.py
@@ -5,5 +5,16 @@ tab1, tab2 = st.tabs(["Automobile", "Football"])
 with tab1 :
   st.header("Automobile")
   st.write("""Depuis le premier jour où je suis montée dans une voiture sportive (Audi RS3), je suis passionnée par le monde automobile.
-  
+  /n Cette passions ayant commencé grâce à un véhicule Audi, cette marque a toujours été ma marque préférée. J'ai toujours suivi les exploit de cette marque, que ca soit dans le monde des constructeur automobile 
+  ou dans le monde du sport automobile. 
+  /n Cependant ma passion à énnormément évolué avec le temps, d'abord en m'intéressant au monde de l'ingénerie automobile, j'ai pue découvrir à quel points Audi avait fait évolué le monde automobile, de l'utilisation d'un 
+  système de quattre roue motrice (FWD), quattro, en série B jusqu'aux phares laser n'éblouïssant pas les conducteur venant d'en face. Mais j'ai aussi pue découvrir que des marques qui m'intéressait moins avaient eux aussi
+  des palmarès d'innovations. Mercedes et l'ABS, McLaren et le fibre de carbone. 
+  /n Ma passions de l'automible est donc devenue la passions des innovations du monde automobile. 
+  /n La majorité de ces innovations étant d'originaires de la compétitions automobile, il etait naturel pour moi de m'y interessé. Et comme prévue, mes marques préférées, les marques investissant le plus dans le recherche et 
+  dévéloppement, les marques à l'origine des plus grandes innovations, ont toujours brillé dans la compétition automobile. 
+  /n En réalité la compétition autobile est devenue la publicité des marques. Les réglementations de ces compétition n'étant pas parfait, les marques ont toujours trouvé un moyen de construire des voitures de courses 
+  bien supérieur aux autres, par exemple la Audi quattro ayant causé la fermeture du groupe B en y regnant comme maitre absolue  ou la première monoplace mercedes équipée de DRS, un système qui permets de modifier la forme 
+  des ailerons,en pleine course sans devoirs s'arreté, pour pouvoirs augmenter ou réduire la traînée aérodynamique, à causé l'interdiction de ce système jusqu'a que les autres écuries puisse le mettre en place, puis lorsque 
+  cela à été fait, les règles des grands prix à changé, ce qui à eu pour conséquences de rendre les grands prix beaucoup plus intense, en augmentant le nombre de dépassement (à part pour la première place). 
   """)
