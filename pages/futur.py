15e0a85661a15a68d7862a35d9cbd2e53f6fc4e1
agitfirat <157006017+agitfirat@users.noreply.github.com>
2024-05-02 10:50:28 +0200

    Update futur.py

--- pages/futur.py
+++ pages/futur.py
@@ -1,11 +1,12 @@
 import streamlit as st 
 
 st.header("FUTUR")
-st.subheader("""'Personne ne peut prédire l'avenir, il est rempli d'incertitudes et de mystères. Ce qui rend la vie excitante, c'est l'inconnu qui nous attend demain.' chatGPT""")
+st.subheader("""'Personne ne peut prédire l'avenir, il est rempli d'incertitudes et de mystères. Ce qui rend la vie excitante, c'est l'inconnu qui nous attend demain.' ChatGPT""")
 
-st.write("""Aujourd'hui l'homme est arrivé au point ou nous arrivons à faire dire ce genre de chose à des ia. 
-         En choisisant cette formation j'avais un but en tête m'orienté ensuite vers le monde de l'ia, je voulais absolument
-         travailler pour un constructeur automobile qui tenterait de construire des voitures autonome. 
-         Aujourd'hui ma vision du monde automobile a énnormément changé, mais j'ai toujours le même rêve.
-         C'est pourquoi j'ai décider de m'orienté vers un master d'ingénieur ia et que """)
+st.write("""Aujourd'hui l'homme est arrivé au point où nous arrivons à faire dire ce genre de chose à des IA. 
+         En choisissant cette formation, j'avais un but en tête : m'orienter ensuite vers le monde de l'IA. Je voulais absolument
+         travailler pour un constructeur automobile qui tenterait de construire des voitures autonomes. 
+         Aujourd'hui, ma vision du monde automobile a énormément changé, mais j'ai toujours le même rêve.
+         C'est pourquoi j'ai décidé de m'orienter vers un master d'ingénieur en IA et que """)
+
 st.subheader("""JE CHERCHE UNE ALTERNANCE EN TANT QUE DATA SCIENTIST. """)
