ec5bb01b00fc2f23b34d54387f7588a940fa50f9
agitfirat <157006017+agitfirat@users.noreply.github.com>
2024-05-02 10:49:18 +0200

    Update passions.py

--- pages/passions.py
+++ pages/passions.py
@@ -1,20 +1,44 @@
 import streamlit as st
 
-tab1, tab2 = st.tabs(["Automobile", "Football"])
+tab1, tab2 = st.columns(2)
 
-with tab1 :
-  st.header("Automobile")
-  st.write("""Depuis le premier jour où je suis montée dans une voiture sportive (Audi RS3), je suis passionnée par le monde automobile.
-  /n Cette passions ayant commencé grâce à un véhicule Audi, cette marque a toujours été ma marque préférée. J'ai toujours suivi les exploit de cette marque, que ca soit dans le monde des constructeur automobile 
-  ou dans le monde du sport automobile. 
-  /n Cependant ma passion à énnormément évolué avec le temps, d'abord en m'intéressant au monde de l'ingénerie automobile, j'ai pue découvrir à quel points Audi avait fait évolué le monde automobile, de l'utilisation d'un 
-  système de quattre roue motrice (FWD), quattro, en série B jusqu'aux phares laser n'éblouïssant pas les conducteur venant d'en face. Mais j'ai aussi pue découvrir que des marques qui m'intéressait moins avaient eux aussi
-  des palmarès d'innovations. Mercedes et l'ABS, McLaren et le fibre de carbone. 
-  /n Ma passions de l'automible est donc devenue la passions des innovations du monde automobile. 
-  /n La majorité de ces innovations étant d'originaires de la compétitions automobile, il etait naturel pour moi de m'y interessé. Et comme prévue, mes marques préférées, les marques investissant le plus dans le recherche et 
-  dévéloppement, les marques à l'origine des plus grandes innovations, ont toujours brillé dans la compétition automobile. 
-  /n En réalité la compétition autobile est devenue la publicité des marques. Les réglementations de ces compétition n'étant pas parfait, les marques ont toujours trouvé un moyen de construire des voitures de courses 
-  bien supérieur aux autres, par exemple la Audi quattro ayant causé la fermeture du groupe B en y regnant comme maitre absolue  ou la première monoplace mercedes équipée de DRS, un système qui permets de modifier la forme 
-  des ailerons,en pleine course sans devoirs s'arreté, pour pouvoirs augmenter ou réduire la traînée aérodynamique, à causé l'interdiction de ce système jusqu'a que les autres écuries puisse le mettre en place, puis lorsque 
-  cela à été fait, les règles des grands prix à changé, ce qui à eu pour conséquences de rendre les grands prix beaucoup plus intense, en augmentant le nombre de dépassement (à part pour la première place). 
-  """)
+with tab1:
+    st.header("Automobile")
+    st.write("""Depuis le premier jour où je suis monté dans une voiture sportive (Audi RS3), je suis passionné par le monde automobile.
+    \n Cette passion a commencé grâce à un véhicule Audi, cette marque a toujours été ma marque préférée. J'ai toujours suivi les exploits de cette marque, que ce soit dans le monde des constructeurs automobiles 
+    ou dans le monde du sport automobile. 
+    \n Cependant ma passion a énormément évolué avec le temps, d'abord en m'intéressant au monde de l'ingénierie automobile, j'ai pu découvrir à quel point Audi avait fait évoluer le monde automobile, de l'utilisation d'un 
+    système de quatre roues motrices (FWD), quattro, en série B jusqu'aux phares laser n'éblouissant pas les conducteurs venant d'en face. Mais j'ai aussi pu découvrir que des marques qui m'intéressaient moins avaient elles aussi
+    des palmarès d'innovations, Mercedes et l'ABS, McLaren et le fibre de carbone. 
+    \n Ma passion pour l'automobile est donc devenue la passion des innovations du monde automobile. 
+    \n La majorité de ces innovations étant d'origine compétitive, il était naturel pour moi de m'y intéresser. Et comme prévu, mes marques préférées, les marques investissant le plus dans la recherche et le 
+    développement, les marques à l'origine des plus grandes innovations, ont toujours brillé dans la compétition automobile. 
+    \n En réalité, la compétition automobile est devenue la publicité des marques. Les réglementations de ces compétitions n'étant pas parfaites, les marques ont toujours trouvé un moyen de construire des voitures de courses 
+    bien supérieures aux autres, par exemple la Audi quattro ayant causé la fermeture du groupe B en y régnant comme maître absolu  ou la première monoplace Mercedes équipée de DRS, un système qui permet de modifier la forme 
+    des ailerons, en pleine course sans devoir s'arrêter, pour pouvoir augmenter ou réduire la traînée aérodynamique, a causé l'interdiction de ce système jusqu'à ce que les autres écuries puissent le mettre en place, puis lorsque 
+    cela a été fait, les règles des grands prix ont changé, ce qui a eu pour conséquence de rendre les grands prix beaucoup plus intenses, en augmentant le nombre de dépassements. 
+    \n Sachant donc que je suis passionné par les innovations du monde automobile, je veux travailler pour un constructeur automobile en tant que data scientist, pour participer, même infiniment, à l'avènement de nouvelles 
+    innovations.
+    """)
+
+with tab2:
+    st.header("Football")
+    st.subheader("'Quel club supportes-tu ?' une question systématiquement posée lorsque deux Turcs se rencontrent pour la première fois.")
+    st.write("""
+    Pour nous les Turcs/Kurdes, le football est bien plus qu'un simple spectacle télévisé ; c'est une passion profondément enracinée dans notre culture. Rencontrer quelqu'un en Turquie signifie souvent découvrir son club 
+    favori parmi les grands noms comme Galatasaray, Besiktas, Trabzon ou Fenerbahce. Et si cette personne est un homme, il y a de fortes chances qu'il soit un "ultra" - un supporter passionné prêt à tout pour son équipe.
+    \n Il m'est donc tout naturel de supporter avec une passion très forte l'un de ses quatre clubs. Lorsque nous devenons assez âgés, 10-12 ans, nous devons choisir le club que nous allons supporter le reste de notre vie.
+    Ce choix est donc très important, en effet dans le cas où nous décidons de suivre la Superlig, notre santé mentale peut dépendre de ce choix. Or un enfant de 10 ans ne peut pas comprendre cela, c'est pourquoi il y a
+    certaines choses qui peuvent nous y aider. Parmi cela nous pouvons citer le suivi d'une personne que l'on aime ou que l'on respecte, soit supporter le même club que cette personne. Cette personne est généralement le père ou
+    un autre membre de la famille, mais nous pouvons aussi citer le palmarès du club, en effet supporter un club qui détient deux fois plus de coupes que ses trois concurrents, est une facilité alors que supporter un club qui
+    n'a pas été sacré champion depuis plus de 10 ans est plus difficile. 
+    \n Pour ma part mon choix s'est dirigé vers le Galatasaray, ni par suivi d'un membre de ma famille, même si nombreux sont les Morcicek supportant le Galatasaray, ni par le palmarès, le Galatasaray est le club turc avec 
+    le plus de victoires en championnats turcs, le seul club turc à avoir gagné une coupe européenne (Europa League + Super coupe) mais par amour au football de Galatasaray. J'adorais le maillot de Galatasaray, c'était le plus
+    beau, si je devais porter un maillot ça devait être lui, les jours étaient très sympathiques, si je devais supporter des joueurs c'étaient eux. Puis avec les années, avec les victoires comme les défaites, avec les arrivants 
+    comme les partants, avec la politique du club, j'ai compris que j'avais fait le bon choix. Encore aujourd'hui je suis fier de supporter un tel club, entré plusieurs fois dans l'histoire. 
+    \n Un petit blason d'une petite ligue, ayant accompli de très grands exploits, sur le terrain comme dans les gradins.
+    \n Voici certains de ces exploits : 
+    \n - Records du plus haut niveau sonore enregistré dans un stade de football (145 décibels)
+    \n - Adversaires du Bayern Munich lors du match le plus compliqué que le Bayern a eu à disputer pendant les phases de poules (Thomas Tuchel)
+    \n - Dernières équipes à avoir remporté un match de Ligue des Champions contre le Real Madrid tout en débutant la deuxième mi-temps en défaveur (2013)
+    """)
